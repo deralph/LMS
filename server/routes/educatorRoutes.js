cce8c1de065d65a4259c078d06ae2b1ae58ee28e
deralph <jraphael441@gmail.com>
2025-08-21 14:19:31 +0100

    corrected middleware error

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,26 +1,29 @@
-import express from 'express'
-import { addCourse, educatorDashboardData, getEducatorCourses, getEnrolledStudentsData, updateRoleToEducator } from '../controllers/educatorController.js';
-import upload from '../configs/multer.js';
-import { protectEducator } from '../middlewares/authMiddleware.js';
+import express from "express";
+import {
+  addCourse,
+  educatorDashboardData,
+  getEducatorCourses,
+  getEnrolledStudentsData,
+  updateRoleToEducator,
+} from "../controllers/educatorController.js";
+import upload from "../configs/multer.js";
 
+const educatorRouter = express.Router();
 
-const educatorRouter = express.Router()
+// Add Educator Role
+educatorRouter.post("/update-role", updateRoleToEducator);
 
-// Add Educator Role 
-educatorRouter.post('/update-role', updateRoleToEducator)
-
-// Add Courses 
+// Add Courses
 // educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse)
-educatorRouter.post('/add-course', upload.single('image'),  addCourse)
+educatorRouter.post("/add-course", upload.single("image"), addCourse);
 
-// Get Educator Courses 
-educatorRouter.post('/courses',  getEducatorCourses)
+// Get Educator Courses
+educatorRouter.post("/courses", getEducatorCourses);
 
 // Get Educator Dashboard Data
-educatorRouter.post('/dashboard',  educatorDashboardData)
+educatorRouter.post("/dashboard", educatorDashboardData);
 
 // Get Educator Students Data
-educatorRouter.post('/enrolled-students',  getEnrolledStudentsData)
-
+educatorRouter.post("/enrolled-students", getEnrolledStudentsData);
 
 export default educatorRouter;
